322710a5be9c8edc30f931aab4458530c8996734
Bernard Villars <bernard@AZ.local>
2026-04-23 15:30:38 +0200

    fix: initialyzedandfichierfirebase

--- JS/APIS/loginFirebase.js
+++ JS/APIS/loginFirebase.js
@@ -1,21 +1,14 @@
 
-import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { setDoc, doc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { db } from "./firebase.js";
 console.log("🔥 fichier Firebase chargé");
 //alert("JS chargé");
-const firebaseConfig = {
-  apiKey: "AIzaSyAKyR8MU6evyhL4qo-fBuza6keMJMzowBg",
-  authDomain: "planning-tech.firebaseapp.com",
-  projectId: "planning-tech",
-  storageBucket: "planning-tech.firebasestorage.app",
-  messagingSenderId: "94701373220",
-  appId: "1:94701373220:web:45e4c04e66ff7677cf55b1"
-};
 
-const app = initializeApp(firebaseConfig);
-console.log("✅ Firebase initialisé");
+//const app = initializeApp(firebaseConfig);
+//console.log("✅ Firebase initialisé");
 //const auth = getAuth(app);
-const db = getFirestore(app);
-console.log("✅ Firestore prêt");
+//const db = getFirestore(app);
+//console.log("✅ Firestore prêt");
 // bouton login
 /* document.getElementById("loginBtn").addEventListener("click", async () => {
     const email = document.getElementById("email").value;
@@ -46,14 +39,15 @@ async function testFirestore() {
             date: new Date().toISOString()
         });
         console.log("✅ écrit en base");
-        alert("Firestore OK 👍");
+        
     } catch (e) {
         console.error(e);
         alert("Erreur Firestore : " + e.message);
     }
 }
 
-window.db = db;
+// optionnel
+window.testFirestore = testFirestore;
 // test login
 /* async function testLogin() {
     try {
--- JS/firebase.js
+++ JS/firebase.js
@@ -2,9 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "...",
-    authDomain: "...",
-    projectId: "..."
+  apiKey: "AIzaSyAKyR8MU6evyhL4qo-fBuza6keMJMzowBg",
+  authDomain: "planning-tech.firebaseapp.com",
+  projectId: "planning-tech",
+  storageBucket: "planning-tech.firebasestorage.app",
+  messagingSenderId: "94701373220",
+  appId: "1:94701373220:web:45e4c04e66ff7677cf55b1"
 };
 
 const app = initializeApp(firebaseConfig);
